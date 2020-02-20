879e5898d300066b12864d34ce8edef025988d81
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-02-20 20:32:46 +0100

    Missed to place a comma

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "Operating System :: POSIX :: Linux",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3 :: Only"
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
