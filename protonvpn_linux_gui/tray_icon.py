6f7e9cd932b4c7124c245aa0a76501bf6e9fe93c
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-04-04 14:37:47 +0200

    Tray icon fully functional

--- protonvpn_linux_gui/tray_icon.py
+++ protonvpn_linux_gui/tray_icon.py
@@ -26,7 +26,7 @@ def indicator(gtk=False):
         itself = True
         gtk = Gtk 
 
-    indicator = appindicator.Indicator.new("ProtonVPN Indicator", "protonvpn-gui", appindicator.IndicatorCategory.APPLICATION_STATUS)
+    indicator = appindicator.Indicator.new("ProtonVPN GUI Indicator", "protonvpn-gui-indicator", appindicator.IndicatorCategory.APPLICATION_STATUS)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(menu(gtk))
     
@@ -37,36 +37,31 @@ def indicator(gtk=False):
         gtk.main()
 
 def connection_status(indicator: complex):
-    icon_path = "/resources/protonvpn_logo_yl.png"
+    icon_path = "/resources/protonvpn_logo_alt.png"
     
     if is_connected():
         icon_path = "/resources/protonvpn_logo.png"
         
-
     indicator.set_icon_full(CURRDIR + icon_path, 'protonvpn')
     return True
 
 def menu(gtk):
     menu = gtk.Menu()
 
-    command_one = gtk.MenuItem(label='Quick Connect')
-    # command_one.connect('activate', note)
-    menu.append(command_one)
+    q_connect = gtk.MenuItem(label='Quick Connect')
+    q_connect.connect('activate', quick_connect)
+    menu.append(q_connect)
 
-    command_one = gtk.MenuItem(label='Disconnect')
-    # command_one.connect('activate', note)
-    menu.append(command_one)
+    disconn = gtk.MenuItem(label='Disconnect')
+    disconn.connect('activate', disconnect)
+    menu.append(disconn)
 
     separator = Gtk.SeparatorMenuItem()
     menu.append(separator)
 
-    command_one = gtk.MenuItem(label='Configurations')
-    # command_one.connect('activate', note)
-    menu.append(command_one)
-
-    command_one = gtk.MenuItem(label='Show')
-    command_one.connect('activate', note)
-    menu.append(command_one) 
+    gui = gtk.MenuItem(label='Show GUI')
+    gui.connect('activate', show_gui)
+    menu.append(gui) 
     
     separator = Gtk.SeparatorMenuItem()
     menu.append(separator)
@@ -78,8 +73,14 @@ def menu(gtk):
     menu.show_all()
     return menu
   
-def note(_):
+def quick_connect(_):
+    subprocess.Popen(["sudo", "protonvpn", "connect", "--fastest"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def show_gui(_):
     subprocess.Popen(["sudo", "protonvpn-gui"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def disconnect(_):
+    subprocess.Popen(["sudo", "protonvpn", "disconnect"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def quit(_, gtk):
     gtk.main_quit()
