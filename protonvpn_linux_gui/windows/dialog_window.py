abf9cb9e51f4aa9749da820426493e2b899eaccd
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-05-05 03:38:08 +0200

    Created new dialog object which will be reused throughout the application

---
+++ protonvpn_linux_gui/windows/dialog_window.py
@@ -0,0 +1,60 @@
+from protonvpn_linux_gui.constants import (
+    UI_DIALOG, 
+)
+
+class DialogWindow: 
+    def __init__(self, interface, Gtk):
+        interface.add_from_file(UI_DIALOG)
+        interface.connect_signals({
+            "close_message_dialog": self.close_message_dialog,
+            "MessageDialog_delete_event": self.MessageDialog_delete_event,
+        })
+
+        self.messagedialog_window = interface.get_object("MessageDialog")
+        self.messagedialog_label = interface.get_object("message_dialog_label")
+        self.messagedialog_spinner = interface.get_object("message_dialog_spinner")
+        self.messagedialog_sub_label = interface.get_object("message_dialog_sub_label")
+        self.messagedialog_sub_label.hide()
+
+        self.message_dialog_close_button = interface.get_object("message_dialog_close_button")
+
+        self.interface = interface 
+        self.gtk = Gtk
+
+    def display_dialog(self, **kwargs):
+        print(kwargs)
+        if "messagedialog_label" in kwargs:
+            self.messagedialog_label.set_markup(kwargs.get("messagedialog_label")) 
+
+        if "messagedialog_sub_label" in kwargs and kwargs.get("messagedialog_sub_label"):
+            self.messagedialog_sub_label.set_markup(kwargs.get("messagedialog_sub_label"))
+            self.messagedialog_sub_label.show()
+        else:
+            self.messagedialog_sub_label.hide()
+
+        if "messagedialog_spinner" in kwargs and kwargs.get("messagedialog_spinner"):
+            self.messagedialog_spinner.show()
+        else:
+            self.messagedialog_spinner.hide()
+
+        if "message_dialog_close_button" in kwargs and kwargs.get("message_dialog_close_button"):
+            self.message_dialog_close_button.hide()
+            self.messagedialog_window.connect("destroy", self.gtk.main_quit)
+        
+        self.messagedialog_window.show()
+
+    def close_message_dialog(self, button):
+        """Button/Event handler to close message dialog.
+        """
+        self.interface.get_object("MessageDialog").hide()
+        
+    # To avoid getting the MessageDialog destroyed and not being re-rendered again
+    def MessageDialog_delete_event(self, window, event):
+        """On Delete handler is used to hide the dialog and so that it successfully renders next time it is called
+        
+        -Returns:Boolean
+        - It needs to return True, otherwise the content will not re-render after closing the window
+        """
+        if window.get_property("visible") is True:
+            window.hide()
+            return True
