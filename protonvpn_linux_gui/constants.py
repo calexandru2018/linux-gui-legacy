daf740ebafd0deae4fa0230a21619ac698cd2865
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-05-18 22:59:06 +0200

    Updated github repo links

--- protonvpn_linux_gui/constants.py
+++ protonvpn_linux_gui/constants.py
@@ -9,8 +9,7 @@ except KeyError:
 
 
 VERSION = "2.0.7"
-
-GITHUB_URL_RELEASE = "https://github.com/calexandru2018/protonvpn-linux-gui/releases/latest"
+GITHUB_URL_RELEASE = "https://github.com/ProtonVPN/protonvpn-linux-gui/releases/latest"
 
 # GUI configurations
 GUI_CONFIG_DIR = os.path.join(os.path.expanduser("~{0}".format(USER)), ".pvpn-gui")
@@ -98,24 +97,6 @@ cd protonvpn-cli-ng
 sudo python3 setup.py install
 """
 HELP_TEXT = """
-<b>How is this GUI related to protonvpn-cli-ng ?</b>
-This GUI works on top of the original cli v{cli_version}. The CLI in this case acts as a dependency for the GUI. 
-All connections are made by invoking the CLI in the background, though all user input is managed by the GUI. 
-
-<b>I am having connectivity issues and I don't know what to do!</b>
-I have a developed a Diagnosis tool that should be helpful in the most cases. You can find the tool under About -> Diagnose.
-This tool will analyse the current configurations and will attempt to help you solve the problem by giving you a brief summary of the root cause.
-It will also provide you some code snippets that you can easily copy/paste into a terminal, even when without internet. If the same issue persists after multiple attempts (to resolve it), try to restart your computer.
-
-<b>I am still experiecing problems, to whom should I create ticket/issue ?</b>
-If you are experiencing any issues, then you should first contact me by opening an issue or any other method. 
-If, after a closer inspection, I notice that the problem originates from within the CLI, then I would then reccomend you to contact protonvpn support instead.
-
-<b>I would like to suggest a feature, where could I do that ?</b>
-If you would like me to implement a new feature or want to give suggestion for one, then you can do it by creating a github issue, select the type of issue, in this case a Feature request.
-Then fill in the template and concisely explain what you would like to see implemented, with as much detail as possible.
-
-<b>I woule like to donate, do you have any e-wallets ?</b>
-First of all, thank you! Any help is very much appreciated and welcome, all this is developed during my free time. If you would like to donate, at the moment I have a liberapay account: https://liberapay.com/calexandru2018
-""".format(cli_version=cli_version)
+<b>To-do</b>
+"""
 
