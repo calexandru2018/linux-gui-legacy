28d913d3c16f40f5cbea485d126fe3d38df9e637
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-06-05 20:12:25 +0200

    Updated to main package name 'app'
    Also updated links to github repo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import re
 import os
 from setuptools import setup
 
-from linux_gui.constants import VERSION
+from app.constants import VERSION
 
 try:
     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
@@ -14,40 +14,40 @@ except FileNotFoundError:
 
     For further information and a usage guide, please view the project page:
 
-    https://github.com/ProtonVPN/linux-gui
+    https://github.com/ProtonVPN/linux-app
     """
 
 setup(
     name="protonvpn-gui",
     packages=[
-        "linux_gui",
-        "linux_gui.presenters",
-        "linux_gui.services",
-        "linux_gui.views",
-        "linux_gui.resources",
-        "linux_gui.resources.img.flags",
-        "linux_gui.resources.img.flags.small",
-        "linux_gui.resources.img.flags.large",
-        "linux_gui.resources.img.logo",
-        "linux_gui.resources.img.utils",
-        "linux_gui.resources.styles",
-        "linux_gui.resources.ui",
+        "app",
+        "app.presenters",
+        "app.services",
+        "app.views",
+        "app.resources",
+        "app.resources.img.flags",
+        "app.resources.img.flags.small",
+        "app.resources.img.flags.large",
+        "app.resources.img.logo",
+        "app.resources.img.utils",
+        "app.resources.styles",
+        "app.resources.ui",
         ],
     entry_points={
             "console_scripts": [
-                "protonvpn-gui = linux_gui.protonvpn_gui:init",
-                "protonvpn-tray = linux_gui.indicator:ProtonVPNIndicator",
+                "protonvpn-app = app.main:init",
+                "protonvpn-tray = app.indicator:ProtonVPNIndicator",
             ]
         },
     include_package_data=True,
     version=VERSION,
-    description="Linux GUI client for ProtonVPN",
+    description="ProtonVPN Application for Linux based OSs",
     long_description=long_descr,
     long_description_content_type="text/markdown",
     author="Proton Technologies AG",
     author_email="contact@protonvpn.com",
     license="GPLv3",
-    url="https://github.com/ProtonVPN/linux-gui",
+    url="https://github.com/ProtonVPN/linux-app",
     install_requires=[
         "protonvpn-cli>=2.2.2",
         "requests>=2.23.0",
